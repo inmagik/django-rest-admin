59761c6322e5417f2efb45c6996650ced8dfebfd
Mauro Bianchi <bianchimro@gmail.com>
2015-11-13 09:23:33 +0100

    comment on urls

--- django_rest_admin/urls.py
+++ django_rest_admin/urls.py
@@ -12,9 +12,11 @@ from rest_framework import routers
 from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
+    # probably would be better to provide our own endpoints.
     url(r'^session-login/', auth_views.login),
     url(r'^session-logout/', auth_views.logout),
     url(r'^token-login/', 'rest_framework.authtoken.views.obtain_auth_token'),
+    
     url(r'^meta/$', RestAdminMetaView.as_view(), name='rest_admin_meta'),
 )
 
